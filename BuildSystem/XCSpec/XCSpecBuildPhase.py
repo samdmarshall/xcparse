c14030b90f9067be76113311921337882ffec529
samdmarshall <me@samdmarshall.com>
2015-03-12 13:57:02 -0400

    removing init calls, using super instead

--- BuildSystem/XCSpec/XCSpecBuildPhase.py
+++ BuildSystem/XCSpec/XCSpecBuildPhase.py
@@ -7,16 +7,4 @@ from .xcspec import *
 class XCSpecBuildPhase(xcspec):
     
     def __init__(self, spec_data):
-        self.contents = spec_data;
-        self.identifier = '';
-        self.type = '';
-        self.name = '';
-        self.basedOn = None;
-        if 'Identifier' in self.keys():
-            self.identifier = self.contents['Identifier'];
-        if 'Type' in self.keys():
-            self.type = self.contents['Type'];
-        if 'Name' in self.keys():
-            self.name = self.contents['Name'];
-        if 'BasedOn' in self.keys():
-            self.basedOn = self.contents['BasedOn'];
+        super(XCSpecBuildPhase, self).__init__(spec_data);
--- BuildSystem/XCSpec/XCSpecCompiler.py
+++ BuildSystem/XCSpec/XCSpecCompiler.py
@@ -7,19 +7,7 @@ from .xcspec import *
 class XCSpecCompiler(xcspec):
     
     def __init__(self, spec_data):
-        self.contents = spec_data;
-        self.identifier = '';
-        self.type = '';
-        self.name = '';
-        self.basedOn = None;
+        super(XCSpecCompiler, self).__init__(spec_data);
         self.abstract = 'NO';
-        if 'Identifier' in self.keys():
-            self.identifier = self.contents['Identifier'];
-        if 'Type' in self.keys():
-            self.type = self.contents['Type'];
-        if 'Name' in self.keys():
-            self.name = self.contents['Name'];
-        if 'BasedOn' in self.keys():
-            self.basedOn = self.contents['BasedOn'];
         if 'IsAbstract' in self.keys():
             self.abstract = self.contents['IsAbstract'];
