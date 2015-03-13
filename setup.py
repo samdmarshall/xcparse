5c52a94644c781073351a22ae5459ee5bbcec667
samdmarshall <me@samdmarshall.com>
2015-03-13 19:26:54 -0400

    updating setup

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ setup(
     license='BSD 3-Clause',
     package_data = {'xcparse': ['defaults.xcconfig']},
     packages=[
-        'xcparse/xcparse', 
-        'xcparse/xcparse/Helpers', 
-        'xcparse/xcparse/Xcode', 
-        'xcparse/xcparse/Xcode/PBX', 
-        'xcparse/xcparse/Xcode/XCSchemeActions', 
-        'xcparse/xcparse/Xcode/BuildSystem', 
-        'xcparse/xcparse/Xcode/BuildSystem/XCSpec', 
-        'xcparse/xcparse/Xcode/BuildSystem/LangSpec',
-        'xcparse/xcparse/Xcode/BuildSystem/Environment'
+        'xcparse', 
+        'xcparse/Helpers', 
+        'xcparse/Xcode', 
+        'xcparse/Xcode/PBX', 
+        'xcparse/Xcode/XCSchemeActions', 
+        'xcparse/Xcode/BuildSystem', 
+        'xcparse/Xcode/BuildSystem/XCSpec', 
+        'xcparse/Xcode/BuildSystem/LangSpec',
+        'xcparse/Xcode/BuildSystem/Environment'
     ],
     zip_safe=False
 )
