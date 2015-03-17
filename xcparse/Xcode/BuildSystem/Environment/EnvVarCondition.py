ba2968163505c9a7fcca4b74d0154951153bb64c
samdmarshall <me@samdmarshall.com>
2015-03-17 07:44:25 -0400

    adding uniqueness to environment variable conditional object

--- xcparse/Xcode/BuildSystem/Environment/EnvVarCondition.py
+++ xcparse/Xcode/BuildSystem/Environment/EnvVarCondition.py
@@ -1,7 +1,5 @@
 class EnvVarCondition(object):
     
-    # this object needs to hash so that we don't get duplicate conditional cases
-    
     def __init__(self, condition_dict, value):
         self.keys = condition_dict.keys();
         self.eval = condition_dict;
@@ -15,3 +13,15 @@ class EnvVarCondition(object):
             print result;
             # break if failed
         return False;
+    
+    def __repr__(self):
+        return '(%s : %s : %s)' % (type(self), self.eval, self.value);
+    
+    def __attrs(self):
+        return (self.keys);
+
+    def __eq__(self, other):
+        return isinstance(other, EnvVarCondition) and self.keys == other.keys;
+
+    def __hash__(self):
+        return hash(self.__attrs())
