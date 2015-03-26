0a4045355bd9892bf06a7cadf0a1ece04eb1f277
samdmarshall <me@samdmarshall.com>
2015-03-26 13:23:16 -0400

    adding objc sample project

---
+++ xcparse-test-objc/xcparse-test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  xcparse-test
+//
+//  Created by Sam Marshall on 3/26/15.
+//  Copyright (c) 2015 Sam Marshall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
